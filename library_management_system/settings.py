1e4cd49bc82adf503b76a146d3789d53d89341b2
Md Imtiaz Sharif <imtiaz2424@gmail.com>
2024-06-22 12:09:16 +0600

    full completed project

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Base url to serve media files
 MEDIA_URL = 'media/'
--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -20,6 +20,7 @@ from core import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
  
@@ -34,6 +35,5 @@ urlpatterns = [
     path('post/', include('posts.urls')),
 ]
 
-
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
